3a73c49161bf5e627686d6fc801783c56135d5bf
P.-.O Caron <41082179+quantmeth@users.noreply.github.com>
2022-05-12 15:06:39 -0400

    Inital commit.

---
+++ _common.R
@@ -0,0 +1,15 @@
+# example R options set globally
+options(width = 60, digits = 2)
+
+# example chunk options set globally
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE
+  )
+
+library(tidyverse)
+library(kableExtra)
+library(tidyr)
+library(papaja)
+library(ggplot2)
+library(rstatix)
